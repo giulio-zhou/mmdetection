57c21b295ecd320472c8611b3d447a5007092c36
Giulio Zhou <giuliozhou8@gmail.com>
2019-01-31 08:54:45 -0800

    Comment out logging config

--- mmdet/apis/env.py
+++ mmdet/apis/env.py
@@ -47,10 +47,10 @@ def set_random_seed(seed):
 
 def get_root_logger(log_level=logging.INFO):
     logger = logging.getLogger()
-    if not logger.hasHandlers():
-        logging.basicConfig(
-            format='%(asctime)s - %(levelname)s - %(message)s',
-            level=log_level)
+    # if not logger.hasHandlers():
+    #     logging.basicConfig(
+    #         format='%(asctime)s - %(levelname)s - %(message)s',
+    #         level=log_level)
     rank, _ = get_dist_info()
     if rank != 0:
         logger.setLevel('ERROR')
